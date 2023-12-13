0eae1febf675b37c425262eadc00e15a9fd9224f
KonateeGrgMorris <konateemorris@gmail.com>
2023-12-13 08:59:52 -0500

    completed variable homework

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/assignments/Variable-Homework.js
@@ -0,0 +1,21 @@
+//SUBMIT A LINK TO THE GITHUB REPOSITORY CONTAINING THIS ASSIGNMENT
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+const isChipmunk = "true"
+const isName = "Papoos"
+const ageInyears = 2
+const heightIninches = 5
+const iscolordarkgray = "true"
+const city = "New York"
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const isWrangler = "true"
+const IsName = "slim"
+const heightInfeet = 6
+const isColordarkachesgray = "true"
+const city = "New York" 
+const state = "NY"
---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/practice/Variable-practice-1.js
@@ -0,0 +1,19 @@
+/*
+  Create a .js file that satisfies assignment below
+  For each exercise you'll want to log the result to the console to check your work
+*/  
+  
+console.log("working")
+  // 1. Declare a new variable and set it equal to the string of your name
+console.log("Eric")
+  // 2. Declare a new variable and set it equal to a number 
+console.log("Konatee")
+  // 3. Set a variable to a boolean value 
+console.log>"Africans = black"<
+  // 4. Create a variable called carName, assign the value Volvo to it.
+console.log("num=50") 
+  // 5. Create a variable called num, assign the value 50 to it.
+  
+  // 6. Display the sum of 5 + 10, using two variables: num1 and num2.
+
+  // 7. Create a variable called num3, assign the addition of num1 and num2 to it.
