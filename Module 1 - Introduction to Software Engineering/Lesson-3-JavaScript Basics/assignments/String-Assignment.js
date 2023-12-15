a2e30765e28e6bb396ba7a2be21c7b6e1eee83fc
KonateeGrgMorris <konateemorris@gmail.com>
2023-12-15 07:29:29 -0500

    numbers hm

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript Basics/assignments/String-Assignment.js
@@ -0,0 +1,105 @@
+
+//SUBMIT A LINK TO THE GITHUB REPOSITORY CONTAINING THIS ASSIGNMENT
+// 1. Concatenate "hello" with "goodbye"
+ const string1 = "hello"
+ const string2  = "goodbye"
+const concatenateString = String1 + string2
+console.log(concatenateString)
+
+// 2. Save the substring "Friend" from the string "Hello Friend"
+const greeting = "Hello Friend"
+const greetingString1 = "Friend"
+const greetingString2 = "Hello Friend"
+const concatenateString1 = greetingString1 + greetingString2
+console.log(concatenateString1)
+
+// 3. Using 5 or more of the following words, create a template literal that makes a legible sentence.
+//    Do not create your own words, use the existing ones below!
+const word1 = "hello"; 
+const word2 = "horror";
+const word3 = "peace";
+const word4 = "world"
+const word5 = "dislike"; 
+const word6 = "umbrella"; 
+const word7 = "car"; 
+const word8 = "pasta"; 
+const word9 = "pizza"; 
+const word10 = "movies";
+const word11 = "football"; 
+const word12 = "soccer"; 
+const word13 = "eat"; 
+const word14 = "cookie"; 
+const word15 = "favorite";
+const word16 = "makeup"; 
+const word17 = "I"; 
+const word18 = "to"; 
+const word19 = "my";
+const word20 = "is";
+const word21 = "and";
+const word22 = "also";
+const word23 = "like"; 
+
+const word1String = "hello"
+const word19String = "my"
+const word4String = "world"
+const word3String = "peace" ;
+const word15String = "favorite"
+const word16String = "makeup" ;
+const concatenateString2 = word1String + word19String + word45String + word35String + word155String + word165String
+console.log(concatenateString2)
+
+const word17String = "I"
+const word23String = "like"
+const word18String = "to"
+const word13String = "eat"
+const word14String = "cookie";
+const word9String = "pizza";
+const word8String = "pasta";
+const concatenateString3 = word175String + word235String + word185String + word135String + word145String + word95String + word85String 
+console.log(concatenateString3)
+
+const word17String2 = "I"
+const word22String = "also"
+const word23String2 = "like"
+const word7String = "car"
+const concatenateString4 = word175String2 + word22String + word23String2 
+console.log(concatenateString4)
+
+const word17String1 = "I"
+const word5String = "dislike"
+const word2String = "horror"
+const word1String1 = "movies";
+const word11String = "football";
+const word12String = "soccer";
+
+const concatenateString5 = word17String1 + word5String + word2String + word1String + word11String + word12String
+console.log(concatenateString5)
+
+// 4. Given the string of your full name get your initials.
+const KonateeString = K
+const GregString = G 
+const MorrisString = M 
+const concatenateString6 = KonateeString + GregString + MorrisString
+console.log(concatenateString6)
+
+// 5. Get the 5th character from the following string: 'Hello World'
+const greeting1 = 'Hello World'
+const greeting1String = nule 
+const concatenateString7 = greeting1String
+console.log(concatenateString7)
+
+// 6. Find the index of the dash from 'Coding-Time!'
+const coding = 'Coding-Time!'
+
+// 7. Write code to grab the final letter from a any string.
+const string = "string"
+
+// 8. Write code to grab the first 3 letters from a string. 
+
+
+// 9. Turn the following string into an array of words: 'This-is-a-sentence';
+const sentence1 = 'This-is-a-sentence'
+
+// 10. Make the string of 12 from the following two numbers:
+let one = 1;
+let two = 2;
